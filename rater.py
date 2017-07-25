5062a9d72fe8d9c5a062a633a46051f63c1cf1a3
DanikNik <nikolsky.dan@gmail.com>
2017-07-26 02:43:01 +0300

    changed parsing engine to bs4 lxml from pandas built-in

--- rater.py
+++ rater.py
@@ -1,29 +1,30 @@
-#!bin/python
+#!/usr/bin/python3
 
 import pandas
+from bs4 import BeautifulSoup
 
 def gen_mirea_table(source):
-        mirea_dataframe= pandas.read_html('raw_pages/{}.html'.format(source), header=0)[0]
+        mirea_dataframe = pandas.read_html('{}.html'.format(source), header=0)[0]
         print('[+]Table {} loaded'.format(source))
         mirea_dataframe.drop([list(mirea_dataframe)[0], list(mirea_dataframe)[4], list(mirea_dataframe)[5], list(mirea_dataframe)[6]], 1, inplace=True)
         print('[+]Table {} formated'.format(source))
         return(mirea_dataframe)
 
 mirea_dataframe_09_03_01 = gen_mirea_table('mirea_09_03_01')
-mirea_dataframe_09_03_02 = gen_mirea_table('mirea_09_03_02')
+#mirea_dataframe_09_03_02 = gen_mirea_table('mirea_09_03_02')
 
-df = pandas.DataFrame()
-k = 0
-print('[+]Output DataFrame initialized')
+#df = pandas.DataFrame()
+#k = 0
+#print('[+]Output DataFrame initialized')
 
-for i in range(len(mirea_dataframe_09_03_01['ФИО'])):
-    fio = mirea_dataframe_09_03_01['ФИО'][i]
-    if fio in mirea_dataframe_09_03_02['ФИО'].values:
-        k+=1
-        print('[+]Found collision', k)
-        row = mirea_dataframe_09_03_01.loc[mirea_dataframe_09_03_01['ФИО']==fio]
-        df = df.append(row, ignore_index=True)
+#for i in range(len(mirea_dataframe_09_03_01['ФИО'])):
+#    fio = mirea_dataframe_09_03_01['ФИО'][i]
+#    if fio in mirea_dataframe_09_03_02['ФИО'].values:
+#        k+=1
+#        print('[+]Found collision', k)
+#        row = mirea_dataframe_09_03_01.loc[mirea_dataframe_09_03_01['ФИО']==fio]
+#        df = df.append(row, ignore_index=True)
 
-with open('rater.html', 'w') as _:
-    _.write(df.to_html())
+with open('rater', 'w') as _:
+    _.write(mirea_dataframe_09_03_01.to_string())
     print('[+]Output file generated')
--- test.py
+++ test.py
@@ -1,8 +1,40 @@
-#!bin/python
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import sys
 import pandas
-frame = pandas.read_html('raw_pages/mpei_01_03_02.html', header=0)[3]
-frame.drop([list(frame)[1], list(frame)[2], list(frame)[3], list(frame)[4], list(frame)[6]], 1, inplace=True)
-frame.drop(0, 0, inplace=True)
-#frame.rename(axis=1)
+from bs4 import BeautifulSoup
+#import requests
+
+with open("mirea_09_03_01.html", "r") as  page:
+    parser = BeautifulSoup(page, 'lxml')
+
+
+fio = []
+doc = []
+agreement = []
+campus = []
+exam = []
+table = parser.find_all("tr")[1:]
+i = 0
+
+for row in table:
+    i+=1
+    col = row.find_all("td")
+    fio.append(col[1].string.strip())
+    doc.append(col[2].string.strip())
+    agreement.append(col[3].string.strip())
+    campus.append(col[4].string.strip())
+    exam.append(col[7].string.strip())
+    sys.stdout.write('\r'+"#"*(50*i//len(table)))
+print("\n[+] Table parsed")
+
+frame = {"fio":fio,
+         "doc":doc,
+         "agreement":agreement,
+         "campus":campus,
+         "exam":exam}
+
+df = pandas.DataFrame(frame)
 with open('test.html', 'w') as _:
-    _.write(frame.to_html())
+    _.write(df.to_html())
