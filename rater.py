2fec20cd084dfb754badbfc71e66f66c9e6cce3d
DanikNik <nikolsky.dan@gmail.com>
2017-07-27 02:47:59 +0300

    added binary search, not giving a fuck what for, i'm so sleepy thus i've just written word 'giving' with a mistake

--- rater.py
+++ rater.py
@@ -1,23 +1,5 @@
 #!/usr/bin/python3
+# -*- coding: utf-8 -*-
 
 import pandas
 import plugin_functions
-
-mirea_dataframe_09_03_01 = plugin_functions.parse_table_mirea('mirea_09_03_01')
-#mirea_dataframe_09_03_02 = gen_mirea_table('mirea_09_03_02')
-
-#df = pandas.DataFrame()
-#k = 0
-#print('[+]Output DataFrame initialized')
-
-#for i in range(len(mirea_dataframe_09_03_01['ФИО'])):
-#    fio = mirea_dataframe_09_03_01['ФИО'][i]
-#    if fio in mirea_dataframe_09_03_02['ФИО'].values:
-#        k+=1
-#        print('[+]Found collision', k)
-#        row = mirea_dataframe_09_03_01.loc[mirea_dataframe_09_03_01['ФИО']==fio]
-#        df = df.append(row, ignore_index=True)
-
-with open('rater', 'w') as _:
-    _.write(mirea_dataframe_09_03_01.to_string())
-    print('[+]Output file generated')
--- test.py
+++ test.py
@@ -1,11 +1,27 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
-from bs4 import BeautifulSoup
-#import requests
+import pandas
 
-with open("input_tables/mpei_01_03_02.html", "r") as  page:
-    parser = BeautifulSoup(page, 'lxml')
+frame = pandas.read_csv('tables/01.03.02_mirea.csv')
+hashtable = []
 
-tables = parser.find_all('table')
-print(len(tables))
+for fio in frame['fio']:
+    hashtable.append([fio, hash(fio)])
+
+def sorting(list_):
+    return list_[1]
+
+hashtable.sort(key=sorting)
+
+def bin_search(table, credentials):
+    print(hash(credentials))
+    left_border = 0
+    right_border = len(lst)
+    while right_border - left_border > 1:
+        index = (left_border + right_border) // 2
+        if hash(credentials) < table[index][1]:
+            right_border = index
+        else:
+            left_border = index
+    return left_border if table[left_border][0] == credentials else None
