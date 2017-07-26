aec812601a5b903492331f8d87acfb28e27fb06c
DanikNik <nikolsky.dan@gmail.com>
2017-07-27 01:59:25 +0300

    cleaned-up some rubbish, polished mirea parsing

--- plugin_functions.py
+++ plugin_functions.py
@@ -3,11 +3,13 @@
 import sys
 import pandas
 from bs4 import BeautifulSoup
-#import requests
+import requests
 
-def parse_table_mirea(source):
-    with open("input_tables/{}.html".format(source), "r") as  page:
-        parser = BeautifulSoup(page, 'lxml')
+with open('urls.txt', 'r') as _:
+    url_list = _.readlines()
+
+def parse_table_mirea(page):
+    parser = BeautifulSoup(page)
     fio = []
     doc = []
     agreement = []
@@ -18,11 +20,14 @@ def parse_table_mirea(source):
     for row in table:
         i+=1
         col = row.find_all("td")
-        fio.append(col[1].string.strip())
-        doc.append(col[2].string.strip())
-        agreement.append(col[3].string.strip())
-        campus.append(col[4].string.strip())
-        exam.append(col[7].string.strip())
+        try:
+            campus.append(col[4].string.strip())
+            fio.append(col[1].string.strip())
+            doc.append(col[2].string.strip())
+            agreement.append(col[3].string.strip())
+            exam.append(col[7].string.strip())
+        except:
+            pass
         sys.stdout.write('\r' + "#"*(70*i//len(table)))
     print("\n[+]Table parsed")
     frame = {"fio":fio,
@@ -32,3 +37,11 @@ def parse_table_mirea(source):
             "exam":exam}
     df = pandas.DataFrame(frame)
     return(df)
+
+if __name__ == '__main__':
+    for url in url_list:
+        page = requests.get(url).text
+        tag = BeautifulSoup(page).find('h1').text.split('\n')[1][:8]
+        print('parsing table ' + tag)
+        data = parse_table_mirea(page)
+        data.to_csv('tables/' + str(tag) + '_mirea.csv')
--- test.py
+++ test.py
@@ -1,40 +1,11 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
-import sys
-import pandas
 from bs4 import BeautifulSoup
 #import requests
 
-with open("mirea_09_03_01.html", "r") as  page:
+with open("input_tables/mpei_01_03_02.html", "r") as  page:
     parser = BeautifulSoup(page, 'lxml')
 
-
-fio = []
-doc = []
-agreement = []
-campus = []
-exam = []
-table = parser.find_all("tr")[1:]
-i = 0
-
-for row in table:
-    i+=1
-    col = row.find_all("td")
-    fio.append(col[1].string.strip())
-    doc.append(col[2].string.strip())
-    agreement.append(col[3].string.strip())
-    campus.append(col[4].string.strip())
-    exam.append(col[7].string.strip())
-    sys.stdout.write('\r'+"#"*(50*i//len(table)))
-print("\n[+] Table parsed")
-
-frame = {"fio":fio,
-         "doc":doc,
-         "agreement":agreement,
-         "campus":campus,
-         "exam":exam}
-
-df = pandas.DataFrame(frame)
-with open('test.html', 'w') as _:
-    _.write(df.to_html())
+tables = parser.find_all('table')
+print(len(tables))
